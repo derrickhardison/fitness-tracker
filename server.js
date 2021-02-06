efa9f683e94a961622ad7ab23a51b840d88414eb
dlhardi <derricklhardison@gmail.com>
2021-02-06 14:10:13 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+ app.use(morgan("dev"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Mongoose connect to deploy into Heroku
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+// Required routes paths
+require("./routes/api.js")(app);
+// require("./routes/html-routes.js")(app);
+
+// Listener
+app.listen(PORT, function() {
+  console.log(`App listening on http://localhost:${PORT}`);
+});
