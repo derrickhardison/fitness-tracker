079a64d0e92f6a9ea482bda5f799cf2a060c66aa
dlhardi <derricklhardison@gmail.com>
2021-02-08 14:40:25 -0500

    Added API routes

---
+++ routes/api-routes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const Workout = require("../models/Workout");
+const Router = express.Router();
+
+Router.post("/api/workouts/", (req, res) => {
+  Workout.create(req.body).then((workout) => {
+    res.json(workout);
+  });
+});
+
+Router.get("/api/workouts/", (req, res) => {
+  Workout.find(req.body).then((workout) => {
+    res.json(workout);
+  });
+});
+
+Router.put("/api/workouts/:id", (req, res) => {
+  const id = req.params.id;
+  Workout.findByIdAndUpdate(
+    id,
+    {
+      $push: { exercises: req.body },
+    },
+    { new: true }
+  ).then((workout) => {
+    res.json(workout);
+  });
+});
+
+module.exports = Router;
